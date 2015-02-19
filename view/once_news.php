9d8f5c832f5ce850283d1f71fc1f444ecc7dc825
Hibiki08 <devilcatt1@gmail.com>
2015-02-19 21:23:59 +0300

    Добавлены переменные

--- view/once_news.php
+++ view/once_news.php
@@ -8,10 +8,10 @@ include 'model\once_news.php';
         <meta charset="utf-8"/>
     </head>
     <body>
-        <h2></h2>
+        <h2><?php  echo $name; ?></h2>
         <h3>Ингредиенты:</h3>
-        <p></p>
+        <p><?php echo $ingredients; ?></p>
         <h3>Приготовление:</h3>
-        <p></p>
+        <p><?php echo $cook; ?></p>
     </body>
 </html>
