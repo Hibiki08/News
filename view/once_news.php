1e6e971953c59e1f4ffff875db6e3b24f6efaf24
Hibiki08 <devilcatt1@gmail.com>
2015-02-19 19:34:15 +0300

    Добавлен и размечен файл once_news.php

---
+++ view/once_news.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title></title>
+        <meta charset="utf-8"/>
+    </head>
+    <body>
+        <h2></h2>
+        <h3>Ингредиенты:</h3>
+        <p></p>
+        <h3>Приготовление:</h3>
+        <p></p>
+    </body>
+</html>
