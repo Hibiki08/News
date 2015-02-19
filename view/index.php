2d42fafce973304ba2cc73c4c277882433cec4bb
Hibiki08 <devilcatt1@gmail.com>
2015-02-19 18:18:48 +0300

    Добавлена форма

--- view/index.php
+++ view/index.php
@@ -4,6 +4,20 @@
         <meta charset="utf-8"/>
     </head>
     <body>
-
+        <form method="post" action="">
+            <label>
+                Название:
+            <input type="text" name="name"/>
+            </label><br/>
+            <label>
+                Ингредиенты:
+            <textarea></textarea>
+            </label><br/>
+            <label>
+                Приготовление:
+            <textarea></textarea>
+            </label><br/>
+            <input type="submit"/>
+        </form>
     </body>
 </html>
