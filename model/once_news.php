65938fd8a6649306e1f2c79d42ba2845ba7850a5
Hibiki08 <devilcatt1@gmail.com>
2015-02-19 21:23:13 +0300

    Добавлен обработчик запроса на новость

--- model/once_news.php
+++ model/once_news.php
@@ -1,2 +1,12 @@
 <?php
 include 'BD_connect.php';
+$arrayBD = Array_BD();
+
+
+if (isset($_GET['id'])) {
+    if ($_GET['id'] == $arrayBD[$_GET['id'] - 1]['id']) {
+        $name = $arrayBD[$_GET['id'] - 1]['Name'];
+        $ingredients = $arrayBD[$_GET['id'] - 1]['Ingredients'];
+        $cook = $arrayBD[$_GET['id'] - 1]['Cook'];
+ }
+}
