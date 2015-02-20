6fbc4f8e01e55face9599827936b19c377524cab
Hibiki08 <devilcatt1@gmail.com>
2015-02-20 14:36:44 +0300

    Изменён include на include_once; Исправлены имена в mysql_query

--- model/add_news.php
+++ model/add_news.php
@@ -1,2 +1,23 @@
 <?php
-var_dump($_POST);
+include_once 'BD_connect.php';
+
+$name = $_POST['name'];
+$ingredients = $_POST['ingredients'];
+$cook = $_POST['cook'];
+
+if ((!empty($_POST['name'])) &&
+    (!empty($_POST['ingredients'])) &&
+    (!empty($_POST['cook']))) {
+
+    BD_connect();
+    mysql_query("INSERT INTO `News`
+    (Name, Ingredients, Cook)
+    VALUES
+    ('$name', '$ingredients', '$cook')");
+}
+elseif ((!empty($_POST)) &&
+    isset($_POST)) {
+    header('Location:'. __DIR__ . '..\index.php'); ?>
+    <p>Не все поля заполнены!</p>
+<?php
+}
